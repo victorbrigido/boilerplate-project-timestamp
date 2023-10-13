b964751cd41235542b1e5fcb4c4e9496ffcff643
Replit user <>
2023-10-13 19:03:56 -0300

    commit microservice

--- index.js
+++ index.js
@@ -1,30 +1,75 @@
-// index.js
-// where your node app starts
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const {MongoClient} = require('mongodb');
+const client = new MongoClient(process.env.DB_URL)
+const db = client.db("urlshortner")
+const urls = db.collection("urls")
 
-// init project
-var express = require('express');
-var app = express();
+const port = process.env.PORT || 3000;
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+
+
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
+}); 
+
+
+app.post('/api/shorturl', function (req, res) {
+console.log(req.body)
+res.json({ greeting: 'hello API'});
+
 });
 
+/*app.get("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.socket.remoteAddress,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent'],
+  })
+}); 
+
+
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date"
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get("/api/:date", function (req, res) {
+  let date = new Date(req.params.date)
 
+  if(isInvalidDate(date)){
+    date = new Date(+req.params.date)
+  }
+  
+  if(isInvalidDate(date)){
+    res.json({error: "Invalid Date"})
+    return;
+  }
+  
+  res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString(),
+    })
+  });
 
+app.get("/api", (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString(),
+    
+  });
+});*/
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
